bd33c2d347cbeeaaed26b702efa63c79288c79b2
Nicholas Kubo <nickskubo@gmail.com>
2021-04-09 14:57:55 -0700

    commit tree 2241f73e78900d877c7b904bd8974ca62a10b5ae

--- .history/lib/sql_queries_20210409114328.rb
+++ .history/lib/sql_queries_20210409114328.rb
@@ -1,29 +1,29 @@
 def selects_all_female_bears_return_name_and_age
-  "SELECT name, age FROM bears WHERE sex = 'F'";
+  "SELECT name, age FROM bears WHERE sex = F"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "SELECT name FROM bears ORDER BY name";
+  "Write your SQL query here"
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "SELECT name, age FROM bears WHERE alive = 1 ORDER BY age";
+  "Write your SQL query here"
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "SELECT name, age FROM bears ORDER BY age DESC LIMIT 1";
+  "Write your SQL query here"
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "SELECT name, age FROM bears ORDER BY age LIMIT 1";
+  "Write your SQL query here"
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT color, COUNT(color) FROM bears GROUP BY color ORDER BY (COUNT(color)) DESC LIMIT 1";
+  "Write your SQL query here"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT temperament, COUNT(temperament)"
+  "Write your SQL query here"
 end
 
 def selects_bear_that_killed_Tim
--- .history/lib/sql_queries_20210409114958.rb
+++ .history/lib/sql_queries_20210409114958.rb
@@ -15,15 +15,15 @@ def selects_oldest_bear_and_returns_name_and_age
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "SELECT name, age FROM bears ORDER BY age LIMIT 1";
+  "Write your SQL query here"
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT color, COUNT(color) FROM bears GROUP BY color ORDER BY (COUNT(color)) DESC LIMIT 1";
+  "Write your SQL query here"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT temperament, COUNT(temperament)"
+  "Write your SQL query here"
 end
 
 def selects_bear_that_killed_Tim
